799d910ee2a1226c230dc803ae18aad13043aa5c
daniel.skroski <daniel@duplod.com.br>
2022-11-01 02:21:05 -0300

    Arquivos React e React Native

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient} from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -11,17 +12,15 @@ async function bootstrap(){
 
     })
 
-    fastify.get('/pools/count', async () => {
-        const pools = await prisma.pool.findMany({ 
-            where: { 
-                code: {
-                    startsWith: 'S'
-                }
-            }
-        })
-        return { pools }
+    await fastify.register(cors, {
+        origin: true,
     })
-    await fastify.listen({ port:3333 })
+
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        return { count }
+    })
+    await fastify.listen({ port:3333, /*host: '0.0.0.0'*/ })
 }
 
 bootstrap()
